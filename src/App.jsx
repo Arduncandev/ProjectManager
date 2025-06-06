3d4005e244a540d697b924204b7a28e0f9843f98
Arduncandev <103537497+Arduncandev@users.noreply.github.com>
2025-06-06 12:25:19 -0500

    added task lists

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,25 @@ function App() {
 	const [projectsState, setProjectsState] = useState({
 		selectedProjectId: undefined,
 		projects: [],
+		tasks: [],
 	});
+
+	function handleAddTask(text) {
+    setProjectsState((prevState) => {
+			const newTask = {
+				text: text,
+        projectId: prevState.selectedProjectId,
+				id: Math.random(),
+			};
+
+			return {
+				...prevState,
+				tasks: [newTask, ...prevState.tasks],
+			};
+		});
+  }
+
+	function handleDeleteTask() {}
 
 	function handleSelectProject(id) {
 		setProjectsState((prevState) => {
@@ -52,19 +70,31 @@ function App() {
 		});
 	}
 
-  function handleDeleteProject() {
-    setProjectsState((prevState) => {
+	function handleDeleteProject() {
+		setProjectsState((prevState) => {
 			return {
 				...prevState,
 				selectedProjectId: undefined,
-        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+				projects: prevState.projects.filter(
+					(project) => project.id !== prevState.selectedProjectId
+				),
 			};
 		});
-  }
+	}
 
-  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
+	const selectedProject = projectsState.projects.find(
+		(project) => project.id === projectsState.selectedProjectId
+	);
 
-	let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
+	let content = (
+		<SelectedProject
+			project={selectedProject}
+			onDelete={handleDeleteProject}
+			onAddTask={handleAddTask}
+			onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+		/>
+	);
 
 	if (projectsState.selectedProjectId === null) {
 		content = (
@@ -72,7 +102,7 @@ function App() {
 		);
 	} else if (projectsState.selectedProjectId === undefined) {
 		content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
-	} 
+	}
 
 	return (
 		<main className='h-screen my-8 flex gap-8'>
@@ -80,7 +110,6 @@ function App() {
 				onStartAddProject={handleStartAddProject}
 				projects={projectsState.projects}
 				onSelectProject={handleSelectProject}
-        
 			/>
 			{content}
 		</main>
