82e2618c19f863af7e640e1c51162d16ecf902a5
Arduncandev <103537497+Arduncandev@users.noreply.github.com>
2025-06-06 12:01:57 -0500

    adds delete functionality

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,4 +1,4 @@
-export default function SelectedProject({ project }) {
+export default function SelectedProject({ project, onDelete }) {
 
     const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
         year: 'numeric',
@@ -7,11 +7,11 @@ export default function SelectedProject({ project }) {
     })
 
     return (
-        <div className="w=[35rem] mt-16">
+        <div className="w-[35rem] mt-16">
             <header className="pb-4 mb-4 border-b-2 border-stone-300">
                 <div className="flex itmes-center justify-between">
                 <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
-                <button className="text-stone-600 hover:text-stone-900">DELETE</button>
+                <button className="text-stone-600 hover:text-stone-900" onClick={onDelete}>DELETE</button>
             </div>
             <p className="mb-4 text-stone-400">{formattedDate}</p>
             <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
